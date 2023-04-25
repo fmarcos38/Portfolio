50131fd07da9f74e7a77728ac8903da726cf55b3
fmarcos38 <fmarcos_23@hotmail.com>
2023-04-25 17:34:39 -0300

    landing

--- src/Components/NavBar/LandingPage/index.js
+++ src/Components/NavBar/LandingPage/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import "./landing.css";
 import Navbar from '..';
+import gif from './program.gif';
 
 function LandingPage() {
     return (
         <div className='contLanding'>
             {/* navbar */}
             <Navbar/>
-            LandingPage
+
+            <div className='contImagTexto'>
+                <div className='contGif'>
+                    <img src={gif} alt='not founf gif' className='gifP'/>
+                </div>
+                <div>
+                    <span className='texto1'>Full Stack Developer</span>
+                </div>
+            </div>
+            
         </div>
     )
 }
