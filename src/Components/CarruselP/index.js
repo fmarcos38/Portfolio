68d52ce5512421188b6e7c948c62ba4736a96559
fmarcos38 <fmarcos_23@hotmail.com>
2023-10-12 16:25:04 -0300

    addNewProy

--- src/Components/CarruselP/index.js
+++ src/Components/CarruselP/index.js
@@ -8,22 +8,22 @@ function CarruselP({imag1, imag2, imag3}) {
                     <div class="carousel-item active">
                         <img src={imag1} class="d-block w-100" alt="..."/>
                     </div>
-    <div class="carousel-item">
-      <img src={imag2} class="d-block w-100" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img src={imag3} class="d-block w-100" alt="..."/>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+                    <div class="carousel-item">
+                        <img src={imag2} class="d-block w-100" alt="..."/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src={imag3} class="d-block w-100" alt="..."/>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
         </div>
     )
 }
